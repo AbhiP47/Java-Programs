70ada1ea217880ca1475bdc9c7bd7598c5a62193
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-08-09 22:49:57 +0530

    synchronization

---
+++ Multi Threading/Mt1/MonitorLocks/Demo2.java
@@ -0,0 +1,34 @@
+package MonitorLocks;
+
+public class Demo2 {
+    public static void main(String[] args) {
+
+        Test test = new Test();
+
+        Thread t1 = new Thread(()->{
+            test.show();
+        });
+
+        Thread t2 = new Thread(()->{
+            test.show();
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Test{
+
+    synchronized  void show()
+    {
+        System.out.println(Thread.currentThread().getName() + " Inside show");
+
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + " Show Finished");
+    }
+}
